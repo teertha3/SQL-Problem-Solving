682a99714bcf12d2eb5edf4f5bca1e3af439ffdb
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-09-25 19:10:05 +0530

    Update second_highest_salary.sql

--- second_highest_salary.sql
+++ second_highest_salary.sql
@@ -1,4 +1,11 @@
+--Using subquery
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary<(SELECT MAX(salary) FROM Employee);
+
+--using IFNULL function
 SELECT IFNULL( 
     (SELECT DISTINCT salary from Employee
     ORDER BY salary DESC LIMIT 1 OFFSET 1),NULL)
 AS SecondHighestSalary;
+
